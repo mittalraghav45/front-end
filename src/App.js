13250eeb2939abce74ef504bbf5e65af8ee6abb7
Raghav Mittal <mittalraghav45@gmail.com>
2025-04-15 19:29:39 +0100

    delete operations

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import SignUp from "./components/SignUp";
 import PrivateComponent from "./components/PrivateComponent";
 import Login from "./components/Login";
 import AddProduct from "./components/AddProduct";
+import ProductList from "./components/ProductList";
 
 function App() {
   return (
@@ -15,7 +16,7 @@ function App() {
         <Routes>
           
           <Route element={<PrivateComponent/>}>
-          <Route path="/" element={<h1>Product Listing Component</h1>}></Route>
+          <Route path="/" element={<ProductList/>}></Route>
           <Route path="/add" element={<AddProduct/>}></Route>
           <Route path="/update" element={<h1>Update Component</h1>}></Route>
           <Route path="/logout" element={<h1>Logout Component</h1>}></Route>
---
+++ src/components/ProductList.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+
+const ProductList = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const getProducts = async () => {
+    let result = await fetch("http://localhost:5000/products");
+    result = await result.json();
+    setProducts(result);
+  };
+  console.log('products ',products);
+  return (
+    <div className="product-list">
+      <h3>Product List</h3> 
+     
+      {
+        products.map((item,index)=>
+            <ul>
+            <li>{index+1}</li>
+            <li>{item.name}</li>
+            <li>$ {item.price}</li>
+            <li>{item.category}</li>       
+          </ul>
+        )
+      }
+    </div>
+  );
+};
+//sfc bank acc details
+//icici bank acc details
+export default ProductList;
